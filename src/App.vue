5f896ab32d0df9808fab1b4f3da8e0c5d4c51b9b
khanhtran2711 <kikact2711@gmail.com>
2026-08-10 11:27:26 +0700

    a bit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <nav style="margin-bottom: 20px;">
       <router-link to="/" style="margin-right: 10px;">Home</router-link>
-      <router-link to="/table">Static Table</router-link>
+      <router-link to="/table">My static table</router-link>
     </nav>
 
     <router-view />
